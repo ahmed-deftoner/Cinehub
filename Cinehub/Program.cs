04b2acadd998c22ee60d2da57de4a70192099601
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-03-24 21:08:58 +0500

    Initial commit

---
+++ Cinehub/Program.cs
@@ -0,0 +1,36 @@
+using Cinehub.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTk3MTcyQDMxMzkyZTM0MmUzME50VkxER2EzWHVKWnZ1NzYzY0Q3UEdRWXRnOW5TeWhsNFNPQXdnYzlKQWM9");
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSyncfusionBlazor();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
