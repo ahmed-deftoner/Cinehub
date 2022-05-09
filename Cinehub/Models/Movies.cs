05962f2d263e4425650e74afea04b119291209c2
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-05-09 19:05:39 +0500

    websockets error removal

---
+++ Cinehub/Models/Movies.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Model
+{
+    public class Movies
+    {
+        public string title { get; set; }
+        public string rating { get; set; }
+        public string desc { get; set; }
+        public string imgUrl { get; set; }
+        public string youtubeUrl { get; set; }
+    }
+}
