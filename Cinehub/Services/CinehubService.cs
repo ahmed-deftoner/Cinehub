c405cfca57093803803e2f849185226b6fbc444b
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-05-19 23:00:58 +0500

    ive lost it

--- Cinehub/Services/CinehubService.cs
+++ Cinehub/Services/CinehubService.cs
@@ -1,6 +1,15 @@
-﻿namespace Cinehub.Services
+﻿using Cinehub.Models;
+
+namespace Cinehub.Services
 {
     public class CinehubService
     {
+        private CinehubContext _context;
+
+        public CinehubService()
+        {
+            _context = new CinehubContext();
+        }
+
     }
 }
